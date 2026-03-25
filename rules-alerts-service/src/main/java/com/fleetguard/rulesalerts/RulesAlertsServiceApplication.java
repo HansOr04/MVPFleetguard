8c3f804044496898d95bf7fff4cabb2eef95076e
HansOr04 <hansalazar04@gmail.com>
2026-03-25 12:54:07 -0500

    refactor: :wrench: Remove maintenance service module, its database, and associated Docker Compose configurations.

---
+++ rules-alerts-service/src/main/java/com/fleetguard/rulesalerts/RulesAlertsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fleetguard.rulesalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesAlertsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RulesAlertsServiceApplication.class, args);
+    }
+}
